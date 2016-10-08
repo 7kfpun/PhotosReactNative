07f5c7fa3ac38ed397117c3d76d7c6a2fa8cd2ec
kf <7kfpun@gmail.com>
2016-10-08 11:34:56 +0800

    Add example config

---
+++ app/config.js
@@ -0,0 +1,16 @@
+exports.config = {
+  admob: {
+    ios: {
+      banner: 'ADMOBID',
+      interstital: 'ADMOBID',
+    },
+    android: {
+      banner: 'ADMOBID',
+      interstital: 'ADMOBID',
+    },
+  },
+  googleAnalytics: {
+    ios: 'TRACKID',
+    android: 'TRACKID',
+  },
+};
